e75c1e8a32a28d42b5b4c985744a7d16c85bb4e7
anVanish <20119316@student.hcmute.edu.vn>
2022-12-14 21:57:09 +0700

    Configure project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
 const express = require('express')
+const hbs = require('express-handlebars')
 const app = express()
 const port = 3000
+const path = require('path')
 
-app.get('/', (req, res)=>{
-    res.send("Hello world")
+const morgan = require('morgan')
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+//use morgan to log the request
+app.use(morgan('combined'))
+
+//template engine
+app.engine('hbs', hbs.engine({
+    extname: ".hbs",
+}))
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'resources/views'))
+
+app.get('/home', (req, res)=>{
+    // res.send("Hello world")
+    res.render('home')
 })
 
-app.listen(port, ()=> console.log(`Web app listening at http://localhost:${port}`))
+app.get('/shop', (req, res)=>{
+    // res.send("Hello world")
+    res.render('shop')
+})
+
+app.listen(port, ()=> console.log(`Web app listening at http://localhost:${port}/home`))
