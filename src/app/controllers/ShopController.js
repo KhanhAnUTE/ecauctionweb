e1ba548061589117302afab770f270e7b446895e
anVanish <20119316@student.hcmute.edu.vn>
2022-12-18 15:19:49 +0700

    add some changes

--- src/app/controllers/ShopController.js
+++ src/app/controllers/ShopController.js
@@ -4,8 +4,10 @@ class ShopController{
 
     //GET /shop
     index(req, res){
+        var user = req.session.user
         res.render('shop', {
             title: "Cửa hàng",
+            user,
         })
     }
 }
